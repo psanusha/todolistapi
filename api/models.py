1291e60a74347a9666a4744f798f9e68a788e387
psanusha <118505694+psanusha@users.noreply.github.com>
2024-03-13 12:02:35 +0530

    updates in task in todo

--- api/models.py
+++ api/models.py
@@ -12,13 +12,18 @@ class Category(models.Model):
         return self.title
 
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ('started', 'Started'),
+        ('ongoing', 'Ongoing'),
+        ('completed', 'Completed'),
+    ]
     title = models.CharField(max_length=200)
     category = models.ForeignKey(Category,blank=True,null=True,on_delete=models.CASCADE)
     description = models.TextField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     started = models.DateTimeField(blank=True, null=True)
     ended = models.DateTimeField(blank=True, null=True)
-    status = models.BooleanField(max_length=20, default='created')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='created')
     created_by = models.ForeignKey(User,on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
 
